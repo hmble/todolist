b70a97b5951f6878fe7eeb7d446753ac086db662
Samyak Bakliwal <w3bcode@gmail.com>
2021-06-01 12:37:06 +0530

    preparing for installation

--- server/db/index.js
+++ server/db/index.js
@@ -1,18 +1,18 @@
 const { Pool, Client } = require('pg')
-const pool = new Pool({
-  user: 'samyak',
-  host: 'localhost',
-  database: 'todolist',
-  password: 'hmblesam',
-  port: 5432,
-})
 // const pool = new Pool({
-//   user: process.env.PG_USER,
-//   host: process.env.PG_HOST,
-//   database: process.env.PG_DATABASE,
-//   password: process.env.PG_PASSWORD,
+//   user: 'samyak',
+//   host: 'localhost',
+//   database: 'todolist',
+//   password: 'hmblesam',
 //   port: 5432,
 // })
+const pool = new Pool({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: 5432,
+})
 
 // module.exports = pool;
 module.exports = {
