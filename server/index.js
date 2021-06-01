ee782785635fa2c725b0e35b55b85ab4e40fa9de
Samyak Bakliwal <w3bcode@gmail.com>
2021-06-01 13:55:40 +0530

    access control allow origin

--- server/index.js
+++ server/index.js
@@ -17,6 +17,14 @@ const port = process.env.PORT || 5000
 // use bodyparser
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
 app.use(express.json())
 app.use('/api', userRoute)
 app.use('/api/todo', middleware.checkauth, todoRoute)
